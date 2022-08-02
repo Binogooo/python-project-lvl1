c4d6ac2dca3c09b350f7c183530cd01e3bc22a68
Binogooo <iluy19991108@mail.ru>
2022-08-02 23:02:42 +0300

    pomogite))

--- brain_games/logic/even_logic.py
+++ brain_games/logic/even_logic.py
@@ -2,22 +2,31 @@ from random import randint
 import prompt
 
 
+def hello():
+    name = prompt.string('May I have your name? ')
+    print(f'Hello, {name}!')
+    print('Answer "yes" if the number is even, otherwise answer "no".')
+
+
 def is_even():
-    random_number = randint(1, 100)
-    print(f'Question: {random_number}')
-    answer = prompt.string('Your answer: ')
-    print(answer_select)
     index = 0
     winscore = 3
-    congrats = print('Congratulations, {name}!')
     while index < winscore:
-        if (random_number % 2 == 0 and answer.lower() == 'yes') and (random_number % 2 != 0 and answer.lower() == 'no'):
-            print('Correct!')
-            index = index + 1
-        elif random_number % 2 != 0 and answer.lower() == 'yes':
-            print('''"yes" is wrong answer ;(. Correct answer was "no".\nLet's try again, {name}''')
+        random_number = randint(1, 100)
+        print(f'Question: {random_number}')
+        answer = prompt.string('Your answer: ')
+        if (random_number % 2 == 0 and answer.lower() == 'yes') or random_number % 2 != 0 and answer.lower() == 'no':
+            print("Correct!")
+            index += 1
         elif random_number % 2 == 0 and answer.lower() == 'no':
-            print('''"yes" is wrong answer ;(. Correct answer was "no".\nLet's try again, {name}''')
+            print("'no' is wrong answer ;(. Correct answer was 'yes'.\nLet's try again, Bill!")
+            break
+        elif random_number % 2 != 0 and answer.lower() == 'yes':
+            print("'yes' is wrong answer ;(. Correct answer was 'no'.\nLet's try again, Bill!")
+            break
         else:
-            print('Wrong answer')
-        return congrats
+            print("Your answer is wrong, try again")
+            break
+        if index == 3:
+            grats = (f'Congratulations, {name}!')
+    return grats
--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -2,14 +2,13 @@
 
 import prompt
 from random import randint
-from  brain_games.logic.even_logic import is_even
+from brain_games.logic.even_logic import is_even 
 
 
 def hello():
     name = prompt.string('May I have your name? ')
     print(f'Hello, {name}!')
     print('Answer "yes" if the number is even, otherwise answer "no".')
-
 
 def main():
     print('Welcome to the Brain Games!')
